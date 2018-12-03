38cd196efda1e123976ee3381389a902741d53d3
Chip <chipz68q@gmail.com>
2018-12-03 16:27:38 +0500

    Add green background for menu

--- src/main_prof.c
+++ src/main_prof.c
@@ -1,7 +1,7 @@
 #include <gtk/gtk.h>
 
 #define	PROGRAM_TITLE	"HH Professional"
-enum {SPACING = 10, WINDOW_WIDTH = 500, WINDOW_HEIGHT = 400};
+enum {SPACING = 10, WINDOW_WIDTH = 525, WINDOW_HEIGHT = 350};
 
 int main(int argc, char *argv[])
 {
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), PROGRAM_TITLE);
-	gtk_container_set_border_width(GTK_CONTAINER(window), SPACING);
+	gtk_container_set_border_width(GTK_CONTAINER(window), 0);
 	gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
 
 	menu = gtk_menu_bar_new();
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	menu_resume_create = gtk_menu_new();
 	menu_root_vacancy = gtk_menu_item_new_with_label("Вакансии");
 	menu_root_resume = gtk_menu_item_new_with_label("Резюме");
-	menu_resume_list = gtk_menu_item_new_with_label("Список");
+	menu_resume_list = gtk_menu_item_new_with_label("Список резюме");
 	menu_resume_root_create = gtk_menu_item_new_with_label("Создать");
 	menu_resume_create_new = gtk_menu_item_new_with_label("Новое");
 	menu_root_test = gtk_menu_item_new_with_label("Test");
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_root_resume);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_root_test);
 
-	vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, SPACING);
+	vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 	gtk_box_pack_start(GTK_BOX(vbox), menu, FALSE, FALSE, 0);
 	gtk_container_add(GTK_CONTAINER(window), vbox);
 
@@ -58,6 +58,8 @@ int main(int argc, char *argv[])
 		gdk_display_get_default_screen(gdk_display_get_default()),
 		GTK_STYLE_PROVIDER(css_provider),
 		GTK_STYLE_PROVIDER_PRIORITY_USER);
+
+	gtk_widget_set_name(menu, "hh_menubar");
 
 	g_signal_connect(G_OBJECT(window), "destroy",
 						G_CALLBACK(gtk_main_quit), NULL);
